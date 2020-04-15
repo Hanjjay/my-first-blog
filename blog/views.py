7671763205156fbe5dcb73528fa5356113c46b02
Hanjay <3354945@gmail.com>
2020-04-16 03:48:03 +0900

    Hanjjay
    add url

--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,6 @@ def post_list(request):
 
 def COVID19_map_data(request):
     return render(request, 'blog/COVID19/COVID_19_in_South_Korea.html', {})
+
+def jays_github(request):
+	return redner(request, 'blog/hanjjay_github/Hanjjay_jay.htm',{})

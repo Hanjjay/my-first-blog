bc3ddacaf80ab9997faf52c13247efc620c69167
Hanjay <3354945@gmail.com>
2020-04-17 21:31:55 +0900

    Hanjjay
    Modified templates to display posts from database

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone         
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 def COVID19_map_data(request):
     return render(request, 'blog/COVID19/COVID_19_in_South_Korea.html', {})
